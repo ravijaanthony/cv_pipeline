77c0fd4556279a48d7df535ea180d165faaac36b
ravijaanthony <ravijaanthony@gmail.com>
2025-03-10 13:55:30 +0530

    fix: minor code changes made

--- backend/server.js
+++ backend/server.js
@@ -342,6 +342,10 @@ app.get("/cv", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send({"server": "running"});
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
